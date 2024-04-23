82de00cf47f1848f9b2b6633459d2a49b65b321f
Shivaprasadr <shivaprasadsmiles@gmail.com>
2024-04-23 13:31:43 +0100

    auth,login,register pytest cover

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,12 +1,9 @@
 from flaskr import create_app
 from flaskr.config import TestConfig
 
-
-
 def test_config():
     assert not create_app().testing
     assert create_app(TestConfig).testing
-
 
 def test_hello(client):
     response = client.get('/hello')
